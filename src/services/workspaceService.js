65274343ffe4b99fa29de24ba445e3e9a6455347
loren pabst <loren pabst>
2017-06-15 15:23:05 -0600

    step 11 done

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import store from './../store.js';
 import apiURL from './../api.js';
-import { showCreateCustomer, createCustomer, getCustomer } from './../ducks/workspaceReducer.js';
+import { showCreateCustomer, createCustomer, getCustomer, updateCustomer, deleteCustomer } from './../ducks/workspaceReducer.js';
 import { dispatchGetList } from './listService.js';
 
 export function dispatchShowCreateCustomer (){
@@ -22,4 +22,21 @@ export function dispatchGetCustomer (id){
     const promise = axios.get( apiURL+id ).then( res => res.data );
 
     store.dispatch( getCustomer(promise) );
+}
+
+export function dispatchUpdateCustomer (id, obj){
+    const promise = axios.patch( apiURL+id, obj ).then( res => {
+        dispatchGetList();
+        return res.data
+    });
+
+    store.dispatch( updateCustomer(promise) );
+}
+
+export function dispatchDeleteCustomer (id){
+    const promise = axios.delete( apiURL+id ).then( res => {
+        dispatchGetList();    
+    });
+
+    store.dispatch( deleteCustomer(promise) );
 }
